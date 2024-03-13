aa42e397680ef3a513bce6ded164b859615cad22
Dilam Polanco <polancodilam6@gmail.com>
2024-03-13 18:17:36 -0500

    First commit backend project

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('./loggerMidleware')
+// const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(logger)
+app.use(cors())
+// const http = require('http')
+
+let notes = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'qui est esse',
+    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+    body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'eum et est occaecati',
+    body: 'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit'
+  }
+]
+
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'application/json' })
+//   response.end(JSON.stringify(notes))
+// })
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log('Llega a funcion')
+  // console.log(note)
+  // if (!note || !note.content) {
+  //   return response.status(400).json({
+  //     error: 'note.content is missing'
+  //   })
+  // }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    title: note.title,
+    body: note.body,
+    userId: note.userId
+  }
+  console.log('Llega aca')
+  notes = notes.concat(newNote)
+  console.log(notes)
+  response.json(newNote)
+})
+
+app.use((request, response) => {
+  console.log(request.path)
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
